d9e428c5b74d945f5580c36805ad5129d318319d
bergd838 <70988659+bergd838@users.noreply.github.com>
2021-04-20 11:59:21 -0400

    Add files via upload

--- studio-project-3/studio-project-3/main.js
+++ studio-project-3/studio-project-3/main.js
@@ -50,14 +50,16 @@ function consoleLogPapermemories() {
   });
 }
 
+// look through our airtable data, create elements
 // loop through the books, create an h2 for each one, and add it to the page
 function showPapermemories() {
   console.log("showPapermemories()");
   papermemories.forEach((papermemory) => {
-   
-   		// var paperName = document.createElement("h1");
-   		// paperName.innerText = papermemory.fields.name;
-   		// document.body.append(paperName)
+
+
+   		// var papermemoryName = document.createElement("h1");
+   		// papermemoryName.innerText = papermemoryname.fields.papername;
+   		// document.body.append(papermemoryName);
 
    		// var papermemoriesNotes = document.createElement("p");
    		// papermemoriesNotes.innerText = papermemory.fields.notes;
@@ -77,8 +79,8 @@ function showPapermemories() {
    		// add name to paper memory
    		var paperName = document.createElement("h1");
    		paperName.classList.add("papermemory-container");
-   		paperName.innerText = papermemory.fields.name;
-   		papermemoryContainer.append("name");
+   		paperName.innerText = papermemory.fields.papername;
+   		papermemoryContainer.append(paperName);
 
    		// add note to paper memory
    		var paperNote = document.createElement("h1");
@@ -88,15 +90,93 @@ function showPapermemories() {
 
    		var papermemoryImage = document.createElement("img");
    		papermemoryImage.classList.add("papermemory-image");
+      // defines what image is being show in the array
    		papermemoryImage.src = papermemory.fields.paperimage[0].url;
    		papermemoryContainer.append(papermemoryImage);
 
    		// add event listener
    		// when user clicks on song container, image and description will appear or disappear
 
-   		papermemoryContainer.addEventListener("click", function(){
-   			papermemoryImage.classList.toggle("active");
-   		})
+   		// papermemoryContainer.addEventListener("click", function(){
+   		// 	papermemoryImage.classList.toggle("active");
+   		// })
+      var ViewModel = function() {
+    var closeupView = this;
+
+    self.showGrid = ko.observable(true);
+    self.toggleView = function() {
+        self.showGrid(!self.showGrid());
+    }
+}
 
   });
 }
+
+ function expandFunction() {
+      // document.querySelector(".papermemories").style.gridTemplateColumns = "1fr";
+      // document.querySelectorAll(".papermemory-image").style.width = "40rem";
+      document.querySelector(".papermemories").style.display = "flex";
+      // document.querySelector(".papermemories").style.overflowX = "scroll";
+      document.querySelector(".papermemories").style.Height = "100vh";
+      document.querySelector(".papermemories").style.width = "auto";
+      document.querySelector(".papermemories").style.gridGap = "40vw";
+      document.querySelector(".papermemories").style.marginLeft = "27%";
+      document.querySelectorAll(".papermemory-image").forEach(el => el.style.width = "30rem");
+      console.log("change color");
+      document.getElementById("previous").style.display = "block";
+      document.getElementById("previous").style.display = "block";
+      document.getElementById("next").style.display = "block";
+
+      document.getElementById('previous').addEventListener('click', function () {
+      document.querySelector('.previous').classList.add('translate');
+});
+      // document.getElementById("demo").addEventListener("click", myFunction);
+}
+
+// function moveBack() {
+//   document.getElementById("previous").style.display = "block";
+// }
+
+      // document.createElement("button");
+      // navigationButton.classList.add("forward-button");
+      // document.querySelector(".forward-button").append(navigationButton);
+      // document.querySelector(".forward-button").style.color = "white"
+      // add event listener to img and change display to none, look at carosal
+  function changeFunction() {
+    document.querySelector(".papermemories").style.display = "grid";
+    document.querySelectorAll(".papermemory-image").forEach(el => el.style.width = "20rem");
+    document.querySelector(".papermemories").style.marginLeft = "9rem";
+    document.querySelector(".papermemories").style.gridGap = ".0701rem";
+    document.getElementById("previous").style.display = "none";
+    document.getElementById("next").style.display = "none";
+
+
+  }
+
+  // function changeDivs() {
+  //   document.querySelector(.papermemories).style.display = "block"
+  // }
+
+  // <button onclick="plusDivs(-1)" class="previous">Previous</button>
+  //     <button onclick="plusDivs(+1)" class="next">Next</button>
+  // function plusDivs() {
+  //   console.log("closeup view");
+
+  //     papermemories.forEach((papermemory) => {
+  //     var singleContainer = document.createElement("div");
+  //     singleContainer.classList.add("single-container");
+  //     document.querySelector(".papermemories").style.display = "grid";
+  //     }
+
+
+
+
+
+
+
+
+
+
+
+
+
