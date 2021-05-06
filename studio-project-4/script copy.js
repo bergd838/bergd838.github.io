2073da4f7872a869baaf875581e0f7b9d1914466
bergd838 <70988659+bergd838@users.noreply.github.com>
2021-05-06 11:15:24 -0400

    Final Studio Project 4!

---
+++ studio-project-4/script copy.js
@@ -0,0 +1,82 @@
+// // console.log("type for art");
+
+// // let yveskleinURL = "assets/yvesklein1.jpeg";
+
+// // let result = document.querySelector("#result");
+
+// // let inputElement = document.querySelector("#input");
+
+// // let result = document.querySelector("#result");
+
+// // inputElement.addEventListener("keydown",function(event){
+
+// // 	if (event.key == "Enter"){
+
+// // 	let color = inputElement.value;
+// //     console.log(color);
+
+// //     inputElement.value = "";
+
+// //     result.innerHTML = "";
+
+// //     for (let i = 0; i < 4; i++){
+// // 		console.log(i);
+
+// // 		let letter = color[i];
+// //       	console.log(color);
+
+// //       	let selectedArt = document.createElement("img");
+
+// //       	if (["b", "l", "u", "e"].includes(letter)){
+// //         selectedArt.src = yveskleinURL;
+// //       }
+
+// //       result.appendChild(selectedArt);
+
+// //       }
+// //     }
+// //   });
+
+// let yveskleinURL = "assets/blue/yvesklein1.jpeg";
+
+// let inputElement = document.querySelector("#input");
+
+// let result = document.querySelector("#result");
+
+// inputElement.addEventListener("keydown",function(event){
+
+//   if (event.key == "Enter"){
+
+
+    
+//     let color = inputElement.value;
+//     console.log(color);
+
+//     inputElement.value = "";
+    
+//     result.innerHTML = "";
+    
+//     for (let i = 0; i < 1; i++){
+      
+//       console.log(i);
+      
+//       let letter = color[i];
+//       console.log(letter);
+      
+//       let selectedArt = document.createElement("img");
+      
+//       if (["b", "l", "u", "e",].includes(letter)){
+//         selectedArt.src = yveskleinURL;
+//       }
+      
+//       result.appendChild(selectedArt);
+      
+//     }
+    
+//   }
+// })
+
+
+
+
+console.log(colors);
